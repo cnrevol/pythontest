4819d645cd86ad0b9dc94c20ba22787089965db0
Jie Song <cnrevol@hotmail.com>
2023-06-06 09:35:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request,render_template
 from azure.storage.filedatalake import DataLakeServiceClient
 import datetime
 import os
+import logging
 
 connstr = os.getenv("AZSTR")
 container = "azc"
@@ -11,9 +12,11 @@ service_client = DataLakeServiceClient.from_connection_string(connstr)
 file_system_client = service_client.get_file_system_client(container)
 
 
-@app.route("/update_blob", methods=["POST"])
+@app.route("/updateblob", methods=["POST"])
 def update_blob():
     data = request.json
+    logging.info(f"POST method update_blob. with json data:{data}")
+    
     message = data.get("message")
     if message:
         current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -24,7 +27,30 @@ def update_blob():
         file_client = file_system_client.create_file(file_name)
         file_client.append_data(content, 0, len(content))
         file_client.flush_data(len(content))
+        logging.info(f"POST method update_blob finished. output file:{file_name}")
+        return "File updated successfully."
 
+    else:
+        return "Invalid request"
+
+@app.route('/updateblob', methods=['GET'])
+def updateblob():
+
+    data = request.args.get('data')
+    
+    logging.info(f"POST method update_blob. with json data:{data}")
+    
+    message = data.get("message")
+    if message:
+        current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        file_name = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        content = f"{current_time}: {message}\n"
+      
+
+        file_client = file_system_client.create_file(file_name)
+        file_client.append_data(content, 0, len(content))
+        file_client.flush_data(len(content))
+        logging.info(f"POST method update_blob finished. output file:{file_name}")
         return "File updated successfully."
 
     else:
